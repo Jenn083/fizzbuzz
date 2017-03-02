7454d7b4f76188e24e7739e936789606b86a60c4
jenn083 <nguyenj1203@gmail.com>
2017-03-01 19:56:11 -0800

    removed strings from integer and added parseInt().

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 
+var theNumStr = prompt('Please input a number');
+var newNum;
 
-var theNum = prompt('Please input a number');
-+theNum;
-var newNum = parseInt(theNum);
+if (isNaN(+theNumStr)){ // 'abc', '123a' //(+) removed strings('') abc, 123a
+  newNum = parseInt(theNumStr); //'abc'==>abc, '123a'==>123
+} else { //1.4, 50
+  newNum = +theNumStr;//do we need? yes, just in case it's '78'
+}
+
+while(isNaN(newNum) || newNum%1 !== 0){// abc, 123a, 1.4 // while condition is true enter the loop
+  theNumStr = prompt('Please input a number');
+  if (isNaN(+theNumStr)){
+    newNum = parseInt(theNumStr);
+  } else { //1.4, 78
+    newNum = +theNumStr;
+  }
+}
+
+// do{
+//   theNumStr = prompt('Please input a number');
+//   if (isNaN(+theNumStr)){
+//     newNum = parseInt(theNumStr);
+//   } else { //1.4, 78
+//     newNum = +theNumStr;
+//   }
+// }while(isNaN(newNum) || newNum%1 !== 0);
 
 
-
-var game = function(i) {
-  for(i = 1; i <= 100; i++){
+var game = function(num) {
+  for(var i = 1; i <= num; i++){
     if(i % 3===0 && i % 5===0) {
       document.write('FizzBuzz' + '<br>');
     }
@@ -18,7 +39,9 @@ var game = function(i) {
       document.write('Fizz' + '<br>');
     }
     else {
-      document.write(i + '<br>');
+      document.write(i + '<br>')
     }
   }
 }
+
+game(newNum);
